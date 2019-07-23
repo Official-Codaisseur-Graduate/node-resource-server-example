38487b8c96cb1993ed68d0e85267f40bb852468b
Billy Vlachos <williamcaminiti@live.com>
2019-07-23 20:19:44 +0200

    Complete Example

---
+++ server.example1.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { promisify } = require('util')
+const { auth, strategies, requiredScopes } = require('express-oauth2-bearer');
+
+const app = express()
+app.use(bodyParser.json());
+
+// process.env.ISSUER_BASE_URL = 'http://localhost:5000';
+// process.env.ALLOWED_AUDIENCES = 'foo';
+
+// Use authentication here
+app.use(auth(strategies.openid({
+    issuerBaseURL: 'http://localhost:5000',
+    allowedAudiences: 'foo'
+})));
+
+// This is an example of an unprotected route
+app.get('/unprotected', (req, res) => {
+    console.log("REACHED UNPROTECTED ENDPOINT, NO AUTH REQUIRED")
+    return res.status(200).send("done")
+})
+
+// This is an example of a protected route
+// that requires the 'read:products' scope
+app.get('/protected-products', requiredScopes('read:products'),
+    (req, res) => {
+        console.log("AUTHENTICATED ROUTE REACHED. USER DETAILS RETRIEVED: ", req.auth)
+        console.dir(req.auth.claims);
+        res.sendStatus(200); z
+    });
+
+const startServer = async () => {
+    const port = process.env.SERVER_PORT || 3000
+    await promisify(app.listen).bind(app)(port)
+    console.log(`Listening on port ${port}`)
+}
+
+startServer()
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { promisify } = require('util')
-const { auth, strategies, requiredScopes } = require('express-oauth2-bearer');
 const authCheck = require('./auth.middleware');
 
 const app = express()
@@ -19,6 +18,15 @@ app.get('/protected', authCheck, (req, res) => {
     console.log("AUTHENTICATED ROUTE REACHED. USER DETAILS RETRIEVED: ", req.user)
     return res.status(200).send("done")
 })
+
+// The default behavior is to throw an error when the token is invalid, 
+// so you can add your custom logic to manage unauthorized access as follows:
+// NOTE: this needs to be added after assigning the routes
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send('invalid token...');
+    }
+});
 
 const startServer = async () => {
     const port = process.env.SERVER_PORT || 3000
